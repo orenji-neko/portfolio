183e31f37278cf111b6cb8adaf7de79083377b0e
orenji-neko <markanthony.enfermo@gmail.com>
2023-05-26 20:50:58 +0800

    Appending some items to gitignore

---
+++ app/src/main/java/org/portfolio/MidtermCodes.java
@@ -0,0 +1,11 @@
+package org.portfolio;
+
+public class MidtermCodes {
+    /**
+     * <h4>Midterm Act 01</h4>
+     * <p>Hello World</p>
+     */
+    public void hello() {
+        System.out.println("Hello World!");
+    }
+}
